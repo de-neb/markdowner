fcdb7118566386d232f3f72b2e6f55b1deab7a22
eden <eedenbaui@gmail.com>
2024-12-03 23:20:45 +0800

    fix: correct tabIndex prop name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,28 +10,14 @@ export default function Navbar() {
   };
 
   return (
-    <div className="navbar bg-base-100 border border-b-slate-200">
+    <div className="navbar bg-base-100 border border-b-slate-200 sticky h-auto top-0 left-0 z-50">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">Markdowner</a>
       </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
           <div
-            tabindex="0"
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabindex="0"
+            tabIndex={0}
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
@@ -40,7 +26,7 @@ export default function Navbar() {
             </div>
           </div>
           <ul
-            tabindex="0"
+            tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
