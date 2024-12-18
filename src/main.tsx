73953ed9d34573ff9f004e5d2359f098efe08df9
eden <eedenbaui@gmail.com>
2024-12-18 22:33:13 +0800

    chore: hide overflow in x-axis of page,

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
-import "./index.css";
 import App from "./App.tsx";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
