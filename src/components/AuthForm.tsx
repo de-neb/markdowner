0ac0520f8966f98080dc6ad16c2e8137154690d8
eden <eedenbaui@gmail.com>
2025-01-14 15:54:04 +0800

    chore: add bg in login page

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -60,7 +60,7 @@ export default function AuthForm() {
     <fetcher.Form
       ref={form}
       onSubmit={handleSubmit}
-      className="flex flex-col gap-4 w-full max-w-sm bg-white/10 rounded-md shadow-lg bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 border border-white/20 p-7"
+      className="flex flex-col gap-4 w-full max-w-sm bg-white/10 rounded-md shadow-lg bg-clip-padding backdrop-filter backdrop-blur-[2px] bg-opacity-0 border border-white/20 p-7"
     >
       <h1 className="text-center text-2xl font-bold">
         {isModeSignup ? "Sign Up" : "Login"}
